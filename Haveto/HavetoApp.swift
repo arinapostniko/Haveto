7e1c5881c621fe2eff9880a80499f1398eae0f6e
Arina <arinapostnikova123@gmail.com>
2023-02-11 16:06:13 +0300

    Initial commit

---
+++ Haveto/HavetoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HavetoApp.swift
+//  Haveto
+//
+//  Created by Arina Postnikova on 11.02.23.
+//
+
+import SwiftUI
+
+@main
+struct HavetoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
