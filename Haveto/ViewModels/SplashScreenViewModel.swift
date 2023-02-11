2484bd9b3b16f119ea956f71715a5328596da237
Arina <arinapostnikova123@gmail.com>
2023-02-11 17:28:43 +0300

    add TabView

---
+++ Haveto/ViewModels/SplashScreenViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SplashScreenViewModel.swift
+//  Haveto
+//
+//  Created by Arina Postnikova on 11.02.23.
+//
+
+import Foundation
---
+++ Haveto/Views/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  Haveto
+//
+//  Created by Arina Postnikova on 11.02.23.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
